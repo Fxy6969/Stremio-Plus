ab19f6fffe1d020e0da2e9f1015073650033a2ca
Fxy <145225725+Fxy6969@users.noreply.github.com>
2026-05-14 13:39:04 +0200

    Cleanup

--- src/Properties.ts
+++
@@ -1,7 +0,0 @@
-class Properties {
-    public static themeLinkSelector: string = "head > link[rel=stylesheet]";
-    public static themesPath = `${process.env.APPDATA}\\stremio-enhanced\\themes`;
-    public static pluginsPath = `${process.env.APPDATA}\\stremio-enhanced\\plugins`;
-}
-
-export default Properties;
